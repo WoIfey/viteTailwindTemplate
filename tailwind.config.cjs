eccfae95a82fca779cab97c3d03645a6bd6cddb6
WoIfey <wuflerz@outlook.com>
2024-01-04 10:26:50 +0200

    Update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.{js,ts,jsx,tsx,html}"],
-  // content: ["./src/**/*.{html,js,ts,jsx,tsx}"], // <- If you want to use React
   theme: {
     extend: {},
   },
